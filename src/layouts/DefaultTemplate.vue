40e878d6c8e6c4c2cccb0293526bb50b37aadee1
Carlos Ferreira <conta@especializati.com.br>
2020-05-21 16:05:32 -0300

    Organized Project Install Initial Dependencies

---
+++ src/layouts/DefaultTemplate.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        dfsds
+    </div>
+</template>
+
+<script>
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+export default {
+    
+}
+</script>
+
+<style scoped>
+@import url('../assets/css/vuefood.css');
+@import url('../assets/css/reset.css');
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-import App from './App.vue'
+import DefaultTemplate from './layouts/DefaultTemplate'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(DefaultTemplate),
 }).$mount('#app')
