af47a4805ed250887929e8da7d3ab3c5643ddd34
Carlos Ferreira <conta@especializati.com.br>
2020-05-26 15:20:50 -0300

    Install and configure vuex

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import BaseTemplate from './layouts/BaseTemplate'
 import router from './routes'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(BaseTemplate),
-  router
+  router,
+  store
 }).$mount('#app')
