306a884a0f759c7f2471ebf1742ae77619e69033
Carlos Ferreira <conta@especializati.com.br>
2020-05-28 10:07:19 -0300

    Create Page Cart Vuex

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -4,79 +4,84 @@
         <div class="card-header text-light">
             <i class="fa fa-shopping-cart" aria-hidden="true"></i>
             Carrinho de Compras
-            <a href="" class="btn btn-outline-info btn-sm pull-right">Continuar Comprando</a>
+            <router-link :to="{ name: 'home' }" class="btn btn-outline-info btn-sm pull-right">
+                Continuar Comprando
+            </router-link>
             <div class="clearfix"></div>
         </div>
         <div class="card-body">
                 <!-- PRODUCT -->
-                <div class="row">
+                <div class="row" v-for="(item, index) in products" :key="index">
                     <div class="col-12 col-sm-12 col-md-2 text-center">
-                            <img class="img-responsive" src="imgs/acai.png" alt="prewiew" width="120" height="80">
+                        <img class="img-responsive" :src="item.product.image" width="120" height="80" :alt="item.product.title">
                     </div>
                     <div class="col-12 text-sm-center col-sm-12 text-md-left col-md-6">
-                        <h4 class="product-name"><strong>Açai Bomba</strong></h4>
+                        <h4 class="product-name"><strong>{{ item.product.title }}</strong></h4>
                         <h4>
-                            <small>Product description</small>
+                            <small>{{ item.product.description }}</small>
                         </h4>
                     </div>
                     <div class="col-12 col-sm-12 text-sm-center col-md-4 text-md-right row">
                         <div class="col-3 col-sm-3 col-md-6 text-md-right" style="padding-top: 5px">
-                            <h6><strong>R$ 12,99 <span class="text-muted">x</span></strong></h6>
+                            <h6><strong>R$ {{ item.product.price }} <span class="text-muted">x</span></strong></h6>
                         </div>
                         <div class="col-4 col-sm-4 col-md-4">
                             <div class="quantity">
-                                <input type="button" value="+" class="plus">
-                                <input type="number" step="1" max="99" min="1" value="1" title="Qty" class="qty"
-                                       size="4">
-                                <input type="button" value="-" class="minus">
+                                <input type="button" value="+" class="plus" @click.prevent="incrementQty(item.product)">
+                                <input type="number" step="1" max="99" min="1" :value="item.qty" title="Qty" class="qty">
+                                <input type="button" value="-" class="minus" @click.prevent="decrementQty(item.product)">
                             </div>
                         </div>
                         <div class="col-2 col-sm-2 col-md-2 text-right">
-                            <button type="button" class="btn btn-outline-danger btn-xs">
+                            <button
+                                type="button"
+                                class="btn btn-outline-danger btn-xs"
+                                @click.prevent="removeCart(item.product)">
                                 <i class="fa fa-trash" aria-hidden="true"></i>
                             </button>
                         </div>
                     </div>
+                    <hr>
                 </div>
-                <hr>
                 <!-- END PRODUCT -->
-                <!-- PRODUCT -->
-                <div class="row">
-                    <div class="col-12 col-sm-12 col-md-2 text-center">
-                            <img class="img-responsive" src="imgs/pizza.png" alt="prewiew" width="120" height="80">
-                    </div>
-                    <div class="col-12 text-sm-center col-sm-12 text-md-left col-md-6">
-                        <h4 class="product-name"><strong>Pizza Salgada</strong></h4>
-                        <h4>
-                            <small>Product description</small>
-                        </h4>
-                    </div>
-                    <div class="col-12 col-sm-12 text-sm-center col-md-4 text-md-right row">
-                        <div class="col-3 col-sm-3 col-md-6 text-md-right" style="padding-top: 5px">
-                            <h6><strong>R$ 12,99 <span class="text-muted">x</span></strong></h6>
-                        </div>
-                        <div class="col-4 col-sm-4 col-md-4">
-                            <div class="quantity">
-                                <input type="button" value="+" class="plus">
-                                <input type="number" step="1" max="99" min="1" value="1" title="Qty" class="qty"
-                                       size="4">
-                                <input type="button" value="-" class="minus">
-                            </div>
-                        </div>
-                        <div class="col-2 col-sm-2 col-md-2 text-right">
-                            <button type="button" class="btn btn-outline-danger btn-xs">
-                                <i class="fa fa-trash" aria-hidden="true"></i>
-                            </button>
-                        </div>
-                    </div>
-                </div>
+
         </div>
         <div class="card-footer card-footer-custom">
             <div class=" text-light" style="margin: 5px">
-                Preço Total: <b>R$ 52,00</b>
+                Preço Total: <b>R$ {{ totalCart }}</b>
             </div>
             <a href="" class="btn btn-success ">Finalizar</a>
         </div>
     </div>
     <!-- cart-->
 </template>
+
+<script>
+import { mapState, mapMutations } from 'vuex'
+
+export default {
+    computed: {
+        ...mapState({
+            products: state => state.cart.products
+        }),
+
+        totalCart () {
+            let total = 0
+
+            this.products.map((itemCart, index) => {
+                total += itemCart.qty * itemCart.product.price
+            })
+
+            return total
+        }
+    },
+
+    methods: {
+        ...mapMutations({
+            removeCart: 'REMOVE_PROD_CART',
+            incrementQty: 'INCREMENT_QTY_PROD_CART',
+            decrementQty: 'DECREMENT_QTY_PROD_CART',
+        }),
+    },
+}
+</script>
